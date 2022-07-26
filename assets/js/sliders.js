79a9ea573771377392778c31369b9e600586ab92
it27427 <istiak.tushar.qtec@gmail.com>
2022-07-26 16:59:25 +0600

    sliders js updated

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,6 +1,6 @@
 /**
  * ABOUT US
- * 
+ * OUR GALLERY CAROUSEL
 **/
 
 
@@ -30,33 +30,3 @@ var swiper = new Swiper(".mySwiper", {
 		prevEl: '.swiper-button-prev',
 	},
 });
-
-// SLIDER START
-// const swiper = new Swiper('.swiper', {
-// 	spaceBetween: 24,
-// 	slidesPerGroup: 1,
-// 	loop: true,
-// 	autoplay: {
-// 		delay: 4000,
-// 		disableOnInteraction: false,
-// 	},
-
-// 	// Navigation arrows
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// 	breakpoints: {
-// 		575: {
-// 			slidesPerView: 2
-// 		},
-
-// 		768: {
-// 			slidesPerView: 2
-// 		},
-
-// 		991: {
-// 			slidesPerView: 3
-// 		},
-// 	},
-// });
