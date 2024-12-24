4e02512030b7985708585d7e93e58b9c73dc6822
Jahir <supersimple.jahir@gmail.com>
2024-12-24 17:17:02 +0600

    [Done] slider issue

--- assets/js/homeclients.js
+++ assets/js/homeclients.js
@@ -33,6 +33,46 @@
 //   },
 // });
 
+// $(".clientLogo").slick({
+//   slidesToShow: 5,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 0,
+//   speed: 2000,
+//   cssEase: "linear",
+//   infinite: true,
+//   responsive: [
+//     {
+//       breakpoint: 1200,
+//       settings: {
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// });
+
 $(".clientLogo").slick({
   slidesToShow: 5,
   slidesToScroll: 1,
@@ -41,6 +81,10 @@ $(".clientLogo").slick({
   speed: 2000,
   cssEase: "linear",
   infinite: true,
+  arrows: false,
+  swipe: false,
+  pauseOnHover: false, // Prevents pausing on hover
+  pauseOnFocus: false, // Prevents pausing on click or focus
   responsive: [
     {
       breakpoint: 1200,
